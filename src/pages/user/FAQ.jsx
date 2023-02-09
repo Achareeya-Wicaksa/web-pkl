ab901c783f1b210d99d42313331088621df4d70b
Frisda Dita <fdisnaini43@gmail.com>
2023-02-09 10:30:37 +0700

    update

--- src/pages/user/FAQ.jsx
+++ src/pages/user/FAQ.jsx
@@ -18,7 +18,7 @@ export default function FAQ() {
             <h1 class="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4 animate-fade animate-once animate-delay-100">
               Frequently Asked Question
             </h1>
-            <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto animate-fade animate-once animate-delay-100">
+            <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
               The most common questions about how our business works and what
               can do for you.
             </p>
@@ -26,59 +26,59 @@ export default function FAQ() {
           <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
             <div class="w-full lg:w-1/2 px-4 py-2">
               <details class="mb-4">
-                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4 break-normal text-black animate-fade-down animate-once animate-ease-linear animate-normal animate-delay-700">
+                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
                 Berapa lama minimal melakukan praktik kerja lapangan pada PT.Angkasa Pura 1 ?
                 </summary>
 
                 <span>
-                Waktu minimal melakukan praktik kerja lapangan adalah 2 bulan.
+                waktu minimal melakukan praktik kerja lapangan adalah 2 bulan.
                 </span>
               </details>
               <details class="mb-4">
-                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4 break-normal text-black animate-fade-down animate-once animate-ease-linear animate-normal animate-delay-700">
-                Berapakah jumlah minimal mahasiswa untuk dapat melakukan praktik kerja lapangan pada PT.Angkasa Pura 1 ?
+                <summary class="font-semibold bg-gray-200 rounded-md py-2 px-4">
+                berapakah jumlah minimal mahasiswa untuk dapat melakukan praktik kerja lapangan pada PT.Angkasa Pura 1 ?
                 </summary>
 
                 <span>
-                Minimal anggota untuk melakukan praktik kerja lapangan adalah 2 orang.
+                minimal anggota untuk melakukan praktik kerja lapangan adalah 2 orang.
                 </span>
               </details>
               <details class="mb-4">
-                <summary class=" font-semibold  bg-gray-200 rounded-md py-2 px-4 break-normal text-black animate-fade-down animate-once animate-ease-linear animate-normal animate-delay-700">
-                Berapa lama surat ajuan akan dibalas / dikonfirmasi?
+                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                berapa lama surat ajuan akan dibalas / dikonfirmasi ?
                 </summary>
 
                 <span>
-                Surat ajuan akan di proses sesuai antrian serta minimal 7 hari kerja.
+                surat ajuan akan di proses sesuai antrian serta minimal 7 hari kerja.
                 </span>
               </details>
             </div>
             <div class="w-full lg:w-1/2 px-4 py-2">
               <details class="mb-4">
-                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4 break-normal text-black animate-fade-down animate-once animate-ease-linear animate-normal animate-delay-700">
-                Apakah ada periode tertentu dalam menerima mahasiswa PKL?
+                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                  blablablabla
                 </summary>
 
                 <span class="px-4 py-2">
-                Tidak ada, selama divisi yang bersangkutan bersedia menerima maka ajuan PKL akan dipertimbangkan. 
+                blablablabla
                 </span>
               </details>
               <details class="mb-4">
-                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4 break-normal text-black animate-fade-down animate-once animate-ease-linear animate-normal animate-delay-700">
-                Project seperti apa yang akan dikerjakan mahasiswa PKL?
+                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                blablablabla
                 </summary>
 
                 <span class="px-4 py-2">
-                Project akan diberitahukan ketika ajuan sudah di terima. 
+                blablablabla
                 </span>
               </details>
               <details class="mb-4">
-                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4 break-normal text-black animate-fade-down animate-once animate-ease-linear animate-normal animate-delay-700">
-                Bagaimana jika surat ajuan yang di kirimkan ternyata salah?
+                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                blablablabla
                 </summary>
 
                 <span class="px-4 py-2">
-                Apabila surat ajuan yang dikirimkan salah maka permohonan PKL tidak akan di proses.
+                blablablabla
                 </span>
               </details>
             </div>
