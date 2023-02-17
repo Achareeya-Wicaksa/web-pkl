197742946ceffaa594a1343500db6dbcc4a39c43
Frisda Dita <fdisnaini43@gmail.com>
2023-02-17 21:23:20 +0700

    updateuserhome

--- src/pages/user/UserHome.jsx
+++ src/pages/user/UserHome.jsx
@@ -67,25 +67,42 @@ export default function UserHome() {
             <Navbar active={"Beranda"} />
 
             <div className="mt-16 px-32 flex flex-col justify-center w-cover h-[680px] bg-cover bg-no-repeat" style={{ background: "url('/home-banner.png')", backgroundSize: "cover", backgroundRepeat: "no-repeat" }}>
-                <b className="break-normal text-6xl text-white animate-fade-down animate-once animate-ease-linear animate-normal animate-delay-700">Penerimaan Online Siswa PKL</b>
-                <span className="mt-6 text-white animate-fade-down animate-once animate-ease-linear animate-normal animate-delay-700">Selamat datang di Website lingkungan PT.Angkasa Pura I (PERSERO) Cabang Bandar Udara Internasional Juanda Surabaya</span>
+                <b className="break-normal text-6xl font-bold text-white animate-fade-down animate-once animate-ease-linear animate-normal animate-delay-700">Penerimaan Online Siswa PKL</b>
+                <span className="mt-6 text-lg font-normal text-white animate-fade-down animate-once animate-ease-linear animate-normal animate-delay-700">Selamat datang di Website lingkungan PT.Angkasa Pura I (PERSERO) Cabang Bandar Udara Internasional Juanda Surabaya</span>
                 <div className="flex flex-row">
-                    <button text={"Daftar"} onClick={() => { navigate("/daftar") }} className="transition ease-in-out delay-150 duration-300 px-6 py-2 mt-12 mr-2 w-fit rounded-md bg-[#35A5D9] hover:bg-white hover:text-[#35A5D9] hover:-translate-y-1 hover:scale-110 transition ease-in-out delay-150 font-normal text-white animate-jump-in animate-once animate-ease-in animate-normal animate-delay-700">Daftar</button>
-                    <a href="#mekanisme" className="transition ease-in-out animate-delay-[1300ms] duration-300 px-6 py-2 mt-12 ml-2 w-fit rounded-md bg-white text-[#35A5D9] hover:bg-[#35A5D9] hover:text-white font-normal animate-jump-in animate-once animate-ease-in animate-normal animate-delay-700">Mekanisme</a>
+                    <button text={"Daftar"} onClick={() => { navigate("/daftar") }} className="transition ease-in-out delay-150 duration-300 px-6 py-2 mt-12 mr-2 w-fit rounded-[20px] bg-[#044895] hover:bg-white hover:text-[#044895] font-medium text-lg hover:-translate-y-1 hover:scale-110 transition ease-in-out delay-150 font-normal text-white animate-jump-in animate-once animate-ease-in animate-normal animate-delay-700">Daftar</button>
+                    <a href="#mekanisme" className="transition ease-in-out animate-delay-[1300ms] duration-300 px-6 py-2 mt-12 ml-2 w-fit rounded-[20px] bg-white text-[#044895] hover:bg-[#044895] hover:text-white font-medium text-lg animate-jump-in animate-once animate-ease-in animate-normal animate-delay-700">Mekanisme</a>
                 </div>
             </div>
 
-           <div data-aos="fade-down" data-aos-offset="200" data-aos-delay="50" data-aos-duration="1000" data-aos-easing="ease-in-out" data-aos-mirror="true" data-aos-once="false" data-aos-anchor-placement="top-center">
-            <div className="px-32 py-12 bg-[#F3FBFF] flex flex-col min-h-[93vh]">
-                <b className="mt-5 text-4xl text-[#35A5D9]">Lowongan Divisi</b>
-                <span className="mt-7">Sebelum melakukan pendaftaran silahkan cek kesesuaian  posisi yang ingin kalian lamar dengan ilmu keahlian kalian</span>
-                <div className="mt-7 flex flex-row grid grid-cols-3 divide-x">
+
+            <div data-aos="fade-down" data-aos-offset="200" data-aos-delay="50" data-aos-duration="1000" data-aos-easing="ease-in-out" data-aos-mirror="true" data-aos-once="false" data-aos-anchor-placement="top-center">           
+                <div class="mx-32 mt-16 mb-16 p-2 font-inter grid grid-cols-1 gap-2 md:grid-cols-2 md:gap-8">
+                    <div class="mt-32 h-14 mb-6 mt-10">
+                        <b className="mb-5 text-4xl text-[#35A5D9]">Company Profile</b>
+                        <h2 class="mb-6 h-10 text-xl font-bold text-slate-600">
+                            <span>PT Angkasa Pura 1 Cabang Juanda Surabaya</span></h2>
+                        <p class="mb-3 h-10 text-sm text-justify text-slate-600">PT Angkasa Pura 1 Cabang Juanda adalah sebuah instansi pemerintah yang bekerja di bidang penerbangan. PT Angkasa Pura 1 Cabang Juanda memberikan
+                        kesempatan kepada para siswa atau mahasiswa yang ingin mengembangkan kemampuannya melalui Praktik Kerja Lapangan (PKL).</p>
+                    </div>
+                    <div class="h-14 mt-32">
+                        <iframe width="480" height="240" src="https://www.youtube.com/embed/eeplXFdctXU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                     </div>  
+            </div>
+            </div>
+            
+
+            <div data-aos="fade-down" data-aos-offset="200" data-aos-delay="50" data-aos-duration="1000" data-aos-easing="ease-in-out" data-aos-mirror="true" data-aos-once="false" data-aos-anchor-placement="top-center">
+            <div className="mt-64 px-16 py-12 bg-[#F3FBFF] flex flex-col min-h-[100vh]">
+                <b className="mx-20 text-4xl text-[#35A5D9]">Lowongan Divisi</b>
+                <span className="mx-20 mt-7">Sebelum melakukan pendaftaran silahkan cek kesesuaian  posisi yang ingin kalian lamar dengan ilmu keahlian kalian</span>
+                    <div class="mx-20 mt-7 p-2 grid grid-cols-1 gap-2 md:grid-cols-3 md:gap-16">
                     {
                         data.map((e, i) => {
                             if (i >= currentPage && i < currentPage + maxPerPage) return (
-                                <div key={i} className="mt-5 bg-white rounded-xl p-16 w-24 h-52 flex flex-col justify-center min-w-[25vw] ">
+                                <div key={i} className="border-2 mt-5 bg-white rounded-xl p-16  sm:w-auto md:w-24 h-auto aspect-[4/3] md:aspect-[3/4] xl:aspect-[4/3] justify-center min-w-[25vw] ">
                                     <h2 class="mb-2 text-lg font-semibold text-[#35A5D9]">{e.title}</h2>
-                                    <ul class="space-y-1 max-w-md list-disc list-inside text-gray-500">
+                                    <ul class="space-y-1 max-w-md list-disc list-inside text-gray-800">
                                         {
                                             e.requirements &&
                                             e.requirements.map((f, j) => {
@@ -101,11 +118,9 @@ export default function UserHome() {
                     }
                 </div>
                 
-                </div>
-
-                <div className="flex w-full items-end justify-end bg-[#F3FBFF] position: sticky pb-10">
-                    <div className="mt-7 mr-10 flex gap-2">
-                        <button className="border rounded w-6 h-6 flex items-center justify-center hover:bg-blue-500" onClick={() => {
+                <div className="flex w-full items-end justify-end">
+                    <div className="mt-7 mx-20 flex gap-2">
+                        <button className="border rounded w-6 h-6 flex items-center justify-center hover:bg-[#044895]" onClick={() => {
                             if (currentPage >= maxPerPage) setCurrentPage(currentPage - maxPerPage)
                         }
                         }><BsChevronLeft /></button>
@@ -116,7 +131,7 @@ export default function UserHome() {
 
                                     let num = i === 0 ? 1 : i / maxPerPage + 1;
                                     return (
-                                        <button className={`border rounded hover:bg-blue-500 ${currentPage === i ? "bg-blue-600 text-white" : ""} w-6 h-6 text-sm`} onClick={() => setCurrentPage(i)}>{num}</button>
+                                        <button className={`border rounded hover:bg-blue-500 ${currentPage === i ? "bg-[#044895] text-white" : ""} w-6 h-6 text-sm`} onClick={() => setCurrentPage(i)}>{num}</button>
                                     )
                                 }
 
@@ -128,12 +143,15 @@ export default function UserHome() {
                         }><BsChevronRight /></button>
                     </div>
                 </div>
+                </div>
+
             </div>
 
-            <div data-aos="fade-up" data-aos-offset="200" data-aos-delay="50" data-aos-duration="1000" data-aos-easing="ease-in-out" data-aos-mirror="true" data-aos-once="false" data-aos-anchor-placement="top-center">
-
-            <div className="px-32 py-12 bg-white flex flex-col items-center justify-center" id="mekanisme">
-                <b className="mt-5 text-4xl text-[#35A5D9]">Mekanisme</b>
+        
+            
+            <div data-aos='fade-down'>
+            <div className="mx-12 my-32 mt-32 px-32 py-12 bg-white flex flex-col items-center justify-center" id="mekanisme">
+                <b className="text-4xl text-[#35A5D9]">Mekanisme</b>
 
                 <div className="mt-7">
                     <span>Berikut adalah tata cara pendaftaran siswa OJT (On the Job Training) / PKL : </span>
@@ -146,18 +164,8 @@ export default function UserHome() {
                     </ol>
                 </div>
             </div>
-
-            <div className="px-32 py-12 bg-white flex flex-col items-center justify-center">
-                <b className="mt-5 text-4xl text-[#35A5D9]">Company Profile</b>
-
-                <div className="mt-7">
-                    <iframe width="720" height="480" src="https://www.youtube.com/embed/eeplXFdctXU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div>
-                <div className="mt-7">
-                    {/* embed the second video in here using width=720 and height 480 */}
-                </div>
             </div>
-            </div>
+
 
             <Footer />
         </div>
