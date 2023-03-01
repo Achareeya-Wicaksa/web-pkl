43628ffa7d9ffc83b70a5cf5930ed47a43cf7911
IceManbrz <81310383+IceManbrz@users.noreply.github.com>
2023-03-01 22:36:03 +0700

    edit btton modal

--- src/pages/user/Pendaftaran.jsx
+++ src/pages/user/Pendaftaran.jsx
@@ -120,15 +120,12 @@ const arr = labels.map((labels,index) =>{
           </div>
         </div>
         <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-          <button type="button" 
+          <button type="button" onClick={() => setShowModal(false)}
           class="inline-flex w-full justify-center rounded-md border 
           border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm 
           hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 
-          sm:ml-3 sm:w-auto sm:text-sm">Hapus</button>
-          <button type="button" onClick={() => setShowModal(false)} class="mt-3 inline-flex w-full justify-center rounded-md 
-          border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 
-          shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 
-          focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Batalkan</button>
+          sm:ml-3 sm:w-auto sm:text-sm">Saya paham</button>
+
         </div>
       </div>
     </div>
