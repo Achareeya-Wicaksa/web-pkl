a114c82cc677fffc8104d28e1e1de0a39de9cda3
IceManbrz <81310383+IceManbrz@users.noreply.github.com>
2023-03-02 15:22:37 +0700

    fix modal

--- src/pages/user/Pendaftaran.jsx
+++ src/pages/user/Pendaftaran.jsx
@@ -104,7 +104,7 @@ var arr = []
           <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true ">
   <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
 
-  <div class="fixed inset-0 z-10 overflow-y-auto ">
+  <div class="fixed inset-0 z-10 overflow-y-auto pt-32">
     <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0 ">
       <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg min-h-20">
         <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
